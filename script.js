1a014c3472619f94f0ff70c02338935149c6b471
Nguyễn Quốc Cường <quoccuongdn96vn@gmail.com>
2019-12-26 04:15:02 +0000

    Hoàn thành ES6

--- script.js
+++ script.js
@@ -102,19 +102,177 @@
 
 // console.log(fun06=());
   
-//Arrow Functions 2
+// //Arrow Functions 2
 
-let course=["android","java","php","angular"];
+// let course=["android","java","php","angular"];
 
-console.log(
-  course.map(course=>{
-    return course.toUpperCase();
-  })
-);
-
-let scores=[9,2,8,4,7,3,1,5,6];
-// scores.sort(
-//   (x,y) => x > y ? true : false
+// console.log(
+//   course.map(course=>{
+//     return course.toUpperCase();
+//   })
 // );
-const newSort=scores.sort();
-console.log(newSort);
+
+// let scores=[9,2,8,4,7,3,1,5,6];
+// scores.sort(
+//  (x,y) => x > y ? 1 : -1
+// );
+// // const newSort=scores.sort();
+// console.log(scores);
+
+// // Từ khóa this
+
+// let student={
+//   name: "John",
+//   courses: ['Android','Java','PHP'],
+//   showInfo: function(){
+//     this.courses.forEach(function(course){
+//       console.log(`John study: ${course}`);
+//     });
+//   }
+// };
+// student.showInfo();
+
+// let student={
+//   name: "John",
+//   courses: ['Android','Java','PHP'],
+//   showInfo: function(){
+//     var _this=this;
+//     this.courses.forEach(function(course){
+//       console.log(`${_this.name} study: ${course}`);
+//     });
+//   }
+// };
+// student.showInfo();
+
+// let student={
+//   name: "Lionel Messi",
+//   courses: ['Android','Java','PHP'],
+//   showInfo: function(){
+//     this.courses.forEach(function(course){
+//       console.log(`${this.name} study: ${course}`);
+//     }.bind(this));
+//   }
+// };
+// student.showInfo();
+
+// let student={
+//   name: "Cristiano Ronaldo",
+//   courses: ['Android','Java','PHP'],
+//   showInfo: function(){
+//     this.courses.forEach((course)=>{
+//       console.log(`${this.name} study: ${course}`);
+//     });
+//   }
+// };
+
+// let student={
+//   name: "Luis Suares",
+//   courses: ['Android','Java','PHP'],
+//   showInfo: function(){
+//     this.courses.forEach(course=>
+//       console.log(`${this.name} study: ${course}`)
+//     );
+//   }
+// };
+// student.showInfo();
+
+//Tìm hiểu Enhanced
+
+//Property value shorthand
+// function getCourse(name,price,free){
+//   return{
+//     name,
+//     price,
+//     free
+//   }
+// };
+// console.log(getCourse("ES6",20,false));
+
+//Method Property
+// function getCourse(name,price,free){
+//   return{
+//     name,
+//     price,
+//     free,
+
+//     showInfo1: function(){
+//       console.log(`${name+" - "+price+" - "+free}`);
+//     },
+
+//     showInfo2(){
+//       console.log(`${name+" - "+price+" - "+free}`);
+//     },
+
+//     showInfo3(delimiter=" - "){
+//       console.log(`${name+delimiter+price+delimiter+free}`);
+//     }
+//   }
+// };
+// var myCourse=getCourse("ES6",20,false);
+// myCourse.showInfo3(" ++++++ ");
+// console.log(myCourse);
+
+//Computed Property Names
+// let beforeBootstrap="Support_Enable";
+// let bootstrap={
+//     [beforeBootstrap+"Chrome"]:true,
+//     [beforeBootstrap+"FrieFox"]:true,
+//     [beforeBootstrap+"IDE"]:false
+// };
+// console.log(bootstrap);
+
+//Sử dụng Class
+// class student{
+//     constructor(code, name, age){
+//         this.code=code;
+//         this.name=name;
+//         this.age=age;
+//     }
+//     getAge(){
+//         let today=new Date();
+//         let year=today.getFullYear();
+//         return year-this.age;
+//     }
+
+//     showInfo(){
+//         return this.code+" - "+this.name+" - "+this.getAge();
+//     }
+// };
+// let studentObj=new student("SV01","Cường",1996);
+// console.log(studentObj);
+// console.log(studentObj.showInfo());
+
+//Sử dụng Inheritance
+
+// class Person{
+//     constructor(name, code, age){
+//         this.name=name;
+//         this.code=code;
+//         this.age=age;
+//     }
+//     getAge(){
+//         let today=new Date();
+//         let year=today.getFullYear();
+//         return year-this.age;
+//     }
+
+//     showInfo(){
+//         return this.name+" - "+this.code+" - "+this.getAge();
+//     }
+// };
+// class Student extends Person{
+//     constructor(name, code, age, score){
+//         super(name, code, age);
+//         this.score=score;
+//     }
+//     showInfo(){
+//         // super.showInfo();
+//         // console.log(" - "+this.score);
+//         return this.name+" - "+this.code+" - "+this.getAge()+" - "+this.score;
+//     }
+// };
+// let personObj= new Person("Cường","201713630",1996);
+// console.log(personObj.showInfo());
+
+// let studentObj=new Student("Bườm","201713631",1997,80);
+// console.log(studentObj.showInfo());
